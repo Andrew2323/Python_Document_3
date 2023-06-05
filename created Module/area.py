d5eeb79cb8966f32192316a1d1ab8a87fe35b9bf
Andrew2323 <sterlingandrew@ymail.com>
2023-06-04 20:11:01 -0500

    first commit

---
+++ created Module/area.py
@@ -0,0 +1,6 @@
+def areaFormula(length, width):
+    return length * width
+
+
+def circleCircumference(radius, pi):
+    return 2 * radius * pi
---
+++ created Module/create_Module.py
@@ -0,0 +1,5 @@
+from area import areaFormula, circleCircumference
+
+print(areaFormula(14, 8))
+
+print(circleCircumference(2, 3.14))
